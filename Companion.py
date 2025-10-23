b63aea766cf84e0f7c0cde12f91007127d918639
Memphis <memphispc70@gmail.com>
2025-10-23 08:37:09 +0200

    Backup

--- Companion.py
+++ Companion.py
@@ -74,12 +74,22 @@ def parallel(entity):
 		return positions
 		
 	planter = Planter.new()
+	planter["set_forcing"](True)
+	planter["set_watering_threshold"](0.75)
+	planter_companion = Planter.new()
 	def execute(position_start):
 		while True:
 			planter["set"](entity)
+			
 			entity_companion, position_companion = get_companion()
+			if abs(position_companion[0] - position_start[0]) >= 3:
+				continue
+			if abs(position_companion[1] - position_start[1]) >= 3:
+				continue
+
 			Move.to(position_companion)
-			planter["set"](entity_companion)
+			planter_companion["set"](entity_companion)
+			
 			Move.to(position_start)
 			if not can_harvest():
 				use_item(Items.Fertilizer)
